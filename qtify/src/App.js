118e8dc57f63427fc3e3ddeea2c34e4706999398
SharmiJohn <1422000joselvi@gmail.com>
2024-03-21 21:51:36 +0530

    header

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,13 @@
-
-
+import React from 'react';
+import {StyledEngineProvider} from '@mui/material/styles';
+import Navbar from "./component/Navbar/Navbar"
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      {/* StyledEngineProvider give more priority for material ui css then add the custom css */}
+       <StyledEngineProvider injectFirst> <Navbar/></StyledEngineProvider>
+     {/* <Navbar/> */}
+     {/* <p>hello world</p> */}
     </div>
   );
 }
