d13132cddec9d14b19c1e4f5a71a718a2ce53e25
SharmiJohn <1422000joselvi@gmail.com>
2024-03-23 19:27:07 +0530

    Hero section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {StyledEngineProvider} from '@mui/material/styles';
 import Navbar from "./component/Navbar/Navbar"
+import Hero from "./component/Hero/Hero"
+
+
+
+
+
+
+
 function App() {
   return (
     <div>
       {/* StyledEngineProvider give more priority for material ui css then add the custom css */}
-       <StyledEngineProvider injectFirst> <Navbar/></StyledEngineProvider>
+       <StyledEngineProvider injectFirst> <Navbar/>
+       <Hero/>
+       </StyledEngineProvider>
+
      {/* <Navbar/> */}
      {/* <p>hello world</p> */}
     </div>
--- qtify/src/index.js
+++ qtify/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom"
+import App from "./App";
+import HomePage from "./component/Pages/HomePage";
+import AlbumPage from "./component/Pages/AlbumPage";
+// router import
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
 
-import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // switch is old version routes is updated version
+    <React.StrictMode>
+      <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/album/albumId" element={<AlbumPage/>} />
+      </Routes>
+      </Router>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
