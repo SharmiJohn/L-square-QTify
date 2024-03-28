7d7d9fcabfb4cfd24056fb8db5c4f494cd90d222
SharmiJohn <1422000joselvi@gmail.com>
2024-03-28 15:05:10 +0530

    Song Section

--- qtify/src/component/Songs/Songs.js
+++ qtify/src/component/Songs/Songs.js
@@ -9,7 +9,8 @@ import Cards from "../Card/Card";
 const Songs = ({ title, data, type, filterfn, songdata }) => {
   const [genre, setgenre] = useState([]);
   const [song, setsong] = useState([]);
-  const [filter, setfilter] = useState([]);
+  const [filters, setfilters] = useState([]);
+  const [value,setvalue]=useState(0);
   //console.log(genre)
   useEffect(() => {
     filterfn().then((response) => {
@@ -21,23 +22,26 @@ const Songs = ({ title, data, type, filterfn, songdata }) => {
     songdata().then((Responsedata) => {
       const songData = Responsedata;
       setsong([...songData]);
+      console.log(song);
+
     });
   }, []);
   //console.log(song);
-  const handleChange = (e) => {
-    let clicktabs = e.target.value;
-
-    console.log(clicktabs);
+  const handleChange = (e,newvalue) => {
+    setvalue(newvalue);
+    let clicktabs=e.target.id;
+   
+   // console.log(clicktabs);
     if (clicktabs === "all") {
-      setfilter([song]);
+      setfilters(song);
       return;
     }
     let fileteredSong = song.filter((ele) => ele.genre.key === clicktabs);
-    setfilter([fileteredSong]);
+    setfilters(fileteredSong);
     return;
   };
-  console.log(filter);
-
+ console.log(filters);
+ 
   return (
     <div>
       <div className={style.title}>
@@ -45,7 +49,8 @@ const Songs = ({ title, data, type, filterfn, songdata }) => {
       </div>
       <div className={style.tab}>
         {" "}
-        <Tabs value={0} onChange={(e) => handleChange(e)}>
+        {/* value 0 means default -1st thing is clicked  */}
+        <Tabs value={value} onChange={handleChange}>
           {genre &&
             genre.map((ele, index) => (
               <Tab
@@ -53,8 +58,11 @@ const Songs = ({ title, data, type, filterfn, songdata }) => {
                   color: "#ffffff",
                   fontFamily: "Poppins",
                   fontWeight: "200",
+                 
+
                 }}
                 label={ele.label}
+                id={ele.key}
                 key={ele.key}
                 value={index}
               />
@@ -62,12 +70,15 @@ const Songs = ({ title, data, type, filterfn, songdata }) => {
         </Tabs>
       </div>
       <div>
-        {filter && (
+        {filters.length!==0 ?(
           <Carousel
-            data={filter}
+            data={filters}
             CardComponent={(data) => <Cards data={data} type={type} />}
           />
-        )}
+        ):<Carousel
+        data={song}
+        CardComponent={(data) => <Cards data={data} type={type} />}
+      />}
       </div>
     </div>
   );
